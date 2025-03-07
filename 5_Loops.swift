b6bdd6208fc7d8a961c1d4d7caa05ff07c9e459e
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2025-03-07 12:38:37 +0530

    Loops

---
+++ 5_Loops.swift
@@ -0,0 +1,42 @@
+// for Loop
+// let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+// for os in platforms {
+//     print("Swift works great on \(os).")
+// }
+
+// for i in 1...12 {
+//     print("5 x \(i) is \(5 * i)")
+// }
+
+
+// while
+// var countdown = 10
+
+// while countdown > 0 {
+//     print("\(countdown)…")
+//     countdown -= 1
+// }
+
+// print("Blast off!")
+
+// let id = Int.random(in: 1...1000)
+
+
+// break and continue
+
+// outerLoop: for option1 in options {
+//     for option2 in options {
+//         for option3 in options {
+//             print("In loop")
+//             let attempt = [option1, option2, option3]
+
+//             if attempt == secretCombination {
+//                 print("The combination is \(attempt)!")
+//                 break outerLoop
+//             }
+//         }
+//     }
+// }
+
+
