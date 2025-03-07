78ed204f894d3e03beaed9bfc95837be1ddc087c
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2025-03-07 12:30:45 +0530

    Operators

--- 3_Operators.swift
+++ 3_Operators.swift
@@ -22,3 +22,25 @@
 
 // Ternary Conditional Operator
 // ?:
+
+
+// Nil-Coalescing Operator
+// var result = result??"false"
+
+
+// Range Operators
+// for index in 1...5 {
+//     print("\(index) times 5 is \(index * 5)")
+// }
+// // 1 times 5 is 5
+// // 2 times 5 is 10
+// // 3 times 5 is 15
+// // 4 times 5 is 20
+// // 5 times 5 is 25
+
+
+// Logical Operators
+// Logical operators modify or combine the Boolean logic values true and false. Swift supports the three standard logical operators found in C-based languages:
+// Logical NOT (!a)
+// Logical AND (a && b)
+// Logical OR (a || b)
