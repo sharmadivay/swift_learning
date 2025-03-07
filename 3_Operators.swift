8ca336d0900cc63fe1e0d1953138539c7998efc4
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2025-03-07 12:27:07 +0530

    Operators

---
+++ 3_Operators.swift
@@ -0,0 +1,24 @@
+// Assignment Operator
+// =
+
+
+// Arithmetic Operators
+// Swift supports the four standard arithmetic operators for all number types:
+// Addition (+)
+// Subtraction (-)
+// Multiplication (*)
+// Division (/)
+
+
+// Comparison Operators
+// Swift supports the following comparison operators:
+// Equal to (a == b)
+// Not equal to (a != b)
+// Greater than (a > b)
+// Less than (a < b)
+// Greater than or equal to (a >= b)
+// Less than or equal to (a <= b)
+
+
+// Ternary Conditional Operator
+// ?:
