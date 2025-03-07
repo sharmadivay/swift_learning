c8d7aef633fc3da76f57b00264a258eb6ef7fbd3
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2025-03-07 12:34:05 +0530

    Conditional

---
+++ 4_Conditionals.swift
@@ -0,0 +1,23 @@
+// If 
+//  If Else 
+//  If Else If Else
+
+
+
+//  Switch Case
+//  let place = "Metropolis"
+// switch place {
+// case "Gotham":
+//     print("You're Batman!")
+// case "Mega-City One":
+//     print("You're Judge Dredd!")
+// case "Wakanda":
+//     print("You're Black Panther!")
+// default:
+//     print("Who are you?")
+// }
+
+
+// Ternary Operator (?:)
+// let age = 18
+// let canVote = age >= 18 ? "Yes" : "No"
